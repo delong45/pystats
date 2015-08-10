b85d8346786cff8815f67757d35d5c6d03f6b267
delong1 <delong1@staff.weibo.com>
2015-08-10 19:03:51 +0800

    new scribe client

--- forkcore.py
+++ forkcore.py
@@ -14,7 +14,7 @@ class Forkcore(object):
         s.listen(5000)
         self.s = s
         self.connections = {}
-        self.incomplete_line = ''
+        self.incomplete_line = {}
         self.unread_line = ''
         self.left_line = ''
         self.time_worker = time_worker
@@ -45,8 +45,9 @@ class Forkcore(object):
             for fd,events in epoll_list:
                 if fd == self.s.fileno():
                     conn,addr = self.s.accept()
-                    self.epoll.register(conn.fileno(), select.EPOLLIN)
+                    self.epoll.register(conn.fileno(), select.EPOLLIN|select.EPOLLET)
                     self.connections[conn.fileno()] = conn
+                    conn.setblocking(0)
                 else:
                     conn = self.connections[fd]
                     self.worker(conn)
@@ -58,23 +59,30 @@ class Forkcore(object):
             self.time_worker(line)
 
     def worker(self, conn):
-        msg = conn.recv(1024)
-        if msg == '':
-            self.epoll.unregister(conn.fileno())
-            self.connections.pop(conn.fileno())    
-            conn.close()
+        while True:
+            try:
+                msg = conn.recv(1024)
+                if msg == '':
+                    self.epoll.unregister(conn.fileno())
+                    self.connections.pop(conn.fileno())    
+                    conn.close()
 
-        if self.incomplete_line:
-            pos = msg.find('\n')
-            self.unread_line = msg[:pos]
-            msg = msg[pos+1:]
-            self.left_line = self.incomplete_line + self.unread_line
-            
-        pos = msg.rfind('\n')
-        self.incomplete_line = msg[pos+1:]
-        msg = msg[:pos]
-        lines = msg.split('\n')
-        for line in lines:
-            self.worker_process(line)
-        self.worker_process(self.left_line)
-        self.left_line = ''
+                if self.incomplete_line.has_key(conn.fileno()) and self.incomplete_line[conn.fileno()]:
+                    pos = msg.find('\n')
+                    self.unread_line = msg[:pos]
+                    msg = msg[pos+1:]
+                    self.left_line = self.incomplete_line[conn.fileno()] + self.unread_line
+                    
+                pos = msg.rfind('\n')
+                self.incomplete_line[conn.fileno()] = msg[pos+1:]
+                msg = msg[:pos]
+                lines = msg.split('\n')
+                for line in lines:
+                    if line[:3] == 'uve':
+                        self.worker_process(line)
+                if self.left_line:
+                    if self.left_line[:3] == 'uve':
+                        self.worker_process(self.left_line)
+                self.left_line = ''
+            except Exception:
+                break
